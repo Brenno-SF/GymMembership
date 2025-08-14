1f32775e018e16119143a4a92a72f2e1e06d4714
Brenno-SF <brprogrammersf@gmail.com>
2025-08-14 13:08:21 -0300

    Initial commit

---
+++ src/main/java/com/bsf/GymMembership/GymMembershipApplication.java
@@ -0,0 +1,11 @@
+package com.bsf.GymMembership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymMembershipApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GymMembershipApplication.class, args);
+	}
+}
