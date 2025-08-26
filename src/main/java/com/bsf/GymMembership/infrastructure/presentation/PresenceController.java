f621cb1f039ac939d730ed800aaf0d793d5544cb
Brenno-SF <brprogrammersf@gmail.com>
2025-08-26 09:38:58 -0300

    refactor: add response entity in presence controller

--- src/main/java/com/bsf/GymMembership/infrastructure/presentation/PresenceController.java
+++ src/main/java/com/bsf/GymMembership/infrastructure/presentation/PresenceController.java
@@ -7,6 +7,8 @@ import com.bsf.GymMembership.core.usecases.presence.ListPresenceByMemberId;
 import com.bsf.GymMembership.infrastructure.exception.NotFoundException;
 import com.bsf.GymMembership.infrastructure.persistence.dto.PresenceDTO;
 import com.bsf.GymMembership.infrastructure.persistence.mapper.PresenceDtoMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,31 +33,31 @@ public class PresenceController {
     }
 
     @PostMapping("/create")
-    public PresenceDTO createPresence(@RequestBody PresenceDTO presenceDTO){
-        return presenceDtoMapper.toDto(
-                presenceCase.execute(presenceDtoMapper.toEntity(presenceDTO))
-        );
+    public ResponseEntity<PresenceDTO> createPresence(@RequestBody PresenceDTO presenceDTO){
+        return ResponseEntity.
+                status(HttpStatus.CREATED).body(presenceDtoMapper.toDto(
+                        presenceCase.execute(presenceDtoMapper.toEntity(presenceDTO))));
     }
 
     @GetMapping("/list/{presenceId}")
-    public PresenceDTO listPresence(@PathVariable UUID presenceId){
-        return presenceDtoMapper.toDto(
-                listPresenceById.execute(presenceId).orElseThrow(()-> new NotFoundException("Presence Not found"))
+    public ResponseEntity<PresenceDTO> listPresence(@PathVariable UUID presenceId){
+        return ResponseEntity.ok(presenceDtoMapper.toDto(
+                listPresenceById.execute(presenceId).orElseThrow(()-> new NotFoundException("Presence Not found")))
         );
     }
     @GetMapping("/listByMember/{memberId}")
-    public List<PresenceDTO> listPresenceByMember(@PathVariable UUID memberId){
-        return listPresenceByMemberId.execute(memberId)
+    public ResponseEntity<List<PresenceDTO>> listPresenceByMember(@PathVariable UUID memberId){
+        return ResponseEntity.ok(listPresenceByMemberId.execute(memberId)
                 .stream()
                 .map(presenceDtoMapper::toDto)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
     }
     @GetMapping("/listByClass/{classId}")
-    public List<PresenceDTO> listPresenceByClass(@PathVariable UUID classId){
-        return listPresenceByClassId.execute(classId)
+    public ResponseEntity<List<PresenceDTO>> listPresenceByClass(@PathVariable UUID classId){
+        return ResponseEntity.ok(listPresenceByClassId.execute(classId)
                 .stream()
                 .map(presenceDtoMapper::toDto)
-                .collect(Collectors.toList());
+                .collect(Collectors.toList()));
     }
 
 }
